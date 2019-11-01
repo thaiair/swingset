a59cbb9935a618a05964383f3a7df4f26fc5fdc6
dan_atiyawijitr <dan.atiyawijitr@hcvt.com>
2019-10-31 21:04:51 -0500

    added authentication for user and pass

--- root/assets/authentication/controllers.js
+++ root/assets/authentication/controllers.js
@@ -13,32 +13,19 @@ angular.module('Authentication')
             $scope.returnData = [];
             $scope.dataLoading = true;
             AuthenticationService.Login($scope.username, $scope.password, function (response) {
-                $scope.returnData = angular.fromJson(response.data);  
-                var val = $scope.returnData.data;
-                console.log(val)                  
-                var returnUsername = function() {
-                     
-                    //return codaData.values
-                    };//values) => values.length > 5);//['c-A4RQAnykpb']
-                //var returnRealUsername = returnUsername.filter((values) => values.length > 1);
-
-                if ($scope.username === 'a' && $scope.password === 'a'){
-                //console.log($scope.returnData);
-                //console.log($scope.returnData.values);
-                    AuthenticationService.SetCredentials($scope.username, $scope.password);
-                    $location.path('/');
+                $scope.returnData = response.data.items;  
+                var isAuthenticated = 0;
+                angular.forEach($scope.returnData, function (item) {
+      
+                   if($scope.username === item.values['c-A4RQAnykpb'] && $scope.password === item.values['c-NwLckyhxEo']) 
+                        isAuthenticated = 1;
+                 });
+                    if (isAuthenticated  === 1) {
+                   AuthenticationService.SetCredentials($scope.username, $scope.password);
+                   $location.path('/');
                 } else {
-                    $scope.error = 'Username or password is incorrect';
-                    $scope.dataLoading = false;
-                    //console.log(angular.fromJson(response.data));
-                 
-                    //if (response.success) {
-                //    AuthenticationService.SetCredentials($scope.username, $scope.password);
-                //    //$scope.content = response.data;
-                //    $location.path('/');
-                //} else {
-                //    $scope.error = response.data//response.message;
-                //    $scope.dataLoading = false;
+                   $scope.error = 'Username or password is incorrect';
+                   $scope.dataLoading = false;
                 }
             });
         };
